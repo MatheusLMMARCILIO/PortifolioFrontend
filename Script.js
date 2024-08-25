085b0e922a7c7ef2591087a318c1f68cd96cf0ba
MatheusLMMARCILIO <142438014+MatheusLMMARCILIO@users.noreply.github.com>
2024-08-25 13:43:58 -0300

    25/08/2024 13h43

--- Script.js
+++ Script.js
@@ -8,20 +8,20 @@ ModoDark.addEventListener("click", () => {
     const description = document.querySelector(".description");
     const menuPart2 = document.querySelector('.menuPart2')
     const tags = document.querySelector(".tags")
-const aboutMeSection = document.querySelector(".aboutMeSection")
-const moreInformation = document.querySelector(".moreInformation")
-const textInformation = document.querySelector(".textInformation")
-const spanSkill = document.querySelector(".spanSkill")
-const html = document.querySelector('.html')
-const css = document.querySelector('.css')
-const JS = document.querySelector('.JS')
-const React = document.querySelector('.React')
-const myProjects = document.querySelector('.myProjects')
-const img = document.querySelector(".IMAGE")
-const contactTitle = document.querySelector(".contactTitle")
-const contactItem = document.querySelector('.contactItem')
-
-
+    const aboutMeSection = document.querySelector(".aboutMeSection")
+    const moreInformation = document.querySelector(".moreInformation")
+    const textInformation = document.querySelector(".textInformation")
+    const spanSkill = document.querySelector(".spanSkill")
+    const html = document.querySelector('.html')
+    const css = document.querySelector('.css')
+    const JS = document.querySelector('.JS')
+    const React = document.querySelector('.React')
+    const myProjects = document.querySelector('.myProjects')
+    const img = document.querySelector(".IMAGE")
+    const contactTitle = document.querySelector(".contactTitle")
+    const contactItem = document.querySelector('.contactItem')
+const IMAGEImg = document.querySelector('.IMAGEImg')
+const IMAGECal = document.querySelector('.IMAGECal')
 
     body.classList.toggle("dark");
     link.classList.toggle("dark");
@@ -40,16 +40,18 @@ const contactItem = document.querySelector('.contactItem')
     React.classList.toggle("dark")
     myProjects.classList.toggle("dark")
     img.classList.toggle("dark")
-contactTitle.classList.toggle('dark')
-contactItem.classList.toggle('dark')
+    contactTitle.classList.toggle('dark')
+    contactItem.classList.toggle('dark')
+    IMAGEImg.classList.toggle('dark')
+    IMAGECal.classList.toggle('dark')
 
 });
 
-document.addEventListener('mousemove', function(event) {
+document.addEventListener('mousemove', function (event) {
     const light = document.getElementById('light');
     const x = event.clientX;
     const y = event.clientY;
 
-    light.style.left = `${x - 50}px`; 
-    light.style.top = `${y - 50}px`; 
+    light.style.left = `${x - 50}px`;
+    light.style.top = `${y - 50}px`;
 });
